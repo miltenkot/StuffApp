828df68516c58515a210759b1c41edd1a7aef9c5
miltenkot <miltenkot@gmail.com>
2020-01-22 23:58:08 +0100

    ✨Add CalendarManager and create LounchScreen

---
+++ StuffApp/View/UserCockpit.swift
@@ -0,0 +1,24 @@
+//
+//  UserCockpit.swift
+//  StuffApp
+//
+//  Created by Bartek Lanczyk on 19/01/2020.
+//  Copyright © 2020 Bartek Lanczyk. All rights reserved.
+//
+
+import SwiftUI
+
+struct UserCockpit: View {
+    var body: some View {
+        NavigationView {
+            Text("cos")
+            
+        }
+    }
+}
+
+struct UserCockpit_Previews: PreviewProvider {
+    static var previews: some View {
+        UserCockpit()
+    }
+}
