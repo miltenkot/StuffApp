450b84d8d364fe9272b78123822b1fafde99ddd0
miltenkot <miltenkot@gmail.com>
2020-02-05 18:56:04 +0100

    ✨Adding navigationlink to seperate UserView from CalendarView

--- StuffApp/View/UserCockpit.swift
+++ StuffApp/View/UserCockpit.swift
@@ -10,67 +10,24 @@ import SwiftUI
 
 struct UserCockpit: View {
     
-    @State var multipleIsPresented = false
-    
-    var rkManager3 = RKManager(calendar: Calendar.current, minimumDate: Date(), maximumDate: Date().addingTimeInterval(60*60*24*365), mode: 3)
-    var rkManager4 = RKManager(calendar: Calendar.current, minimumDate: Date(), maximumDate: Date().addingTimeInterval(60*60*24*365), mode: 0)
+    @State var flag = true
     
     var body: some View {
-        VStack {
-            HStack {
+        NavigationView{
+            VStack(){
                 Image("portret")
-                    .scaleEffect(0.5)
+                    .imageScale(.medium)
                     .cornerRadius(150)
-                Spacer()
-            Button(action: { self.multipleIsPresented.toggle() }) {
-                Text("Wnioski").foregroundColor(.blue)
-            }
-            .sheet(isPresented: self.$multipleIsPresented, content: {
-                RKViewController(isPresented: self.$multipleIsPresented, rkManager: self.rkManager3)})
-            
-            //datesView(dates: self.rkManager3.selectedDates)
-            }
-        }.onAppear(perform: startUp)
-            .navigationViewStyle(StackNavigationViewStyle())
-        .padding(60)
-        
-    }
-    func datesView(dates: [Date]) -> some View {
-        ScrollView (.horizontal) {
-            HStack {
-                ForEach(dates, id: \.self) { date in
-                    Text(self.getTextFromDate(date: date))
+                HStack {
+                    NavigationLink(destination: UserCalendar()){
+                        Text("Wnioski")
+                    }
+                    
                 }
             }
-        }.padding(.horizontal, 15)
-        
-    }
-    
-    func startUp() {
-        
-        // example of pre-setting selected dates
-//        let testOnDates = [Date().addingTimeInterval(60*60*24), Date().addingTimeInterval(60*60*24*2)]
-//        rkManager3.selectedDates.append(contentsOf: testOnDates)
-        
-        // example of some foreground colors
-        rkManager3.colors.weekdayHeaderColor = Color.blue
-        rkManager3.colors.monthHeaderColor = Color.green
-        rkManager3.colors.textColor = Color.blue
-        rkManager3.colors.disabledColor = Color.red
-        
-        // example of pre-setting disabled dates
-//        let testOffDates = [
-//            Date().addingTimeInterval(60*60*24*4),
-//            Date().addingTimeInterval(60*60*24*5),
-//            Date().addingTimeInterval(60*60*24*7)]
-//        rkManager4.disabledDates.append(contentsOf: testOffDates)
-    }
-    
-    func getTextFromDate(date: Date!) -> String {
-        let formatter = DateFormatter()
-        formatter.locale = .current
-        formatter.dateFormat = "EEEE, MMMM d, yyyy"
-        return date == nil ? "" : formatter.string(from: date)
+            
+            
+        }
     }
 }
 
@@ -79,3 +36,8 @@ struct UserCockpit_Previews: PreviewProvider {
         UserCockpit()
     }
 }
+
+//    Image("portret")
+//                   .scaleEffect(0.5)
+//                   .cornerRadius(150)
+//Text("Wnioski").foregroundColor(.blue)
